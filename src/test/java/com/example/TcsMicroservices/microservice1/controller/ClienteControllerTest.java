427e86bbde7da2ebe517b735b983fa8b635519d2
Jimmy Palma <jimmypalma@Jimmys-MacBook-Pro.local>
2025-05-18 16:09:32 -0500

    remove unused imports

--- src/test/java/com/example/TcsMicroservices/microservice1/controller/ClienteControllerTest.java
+++ src/test/java/com/example/TcsMicroservices/microservice1/controller/ClienteControllerTest.java
@@ -1,14 +1,11 @@
 package com.example.TcsMicroservices.microservice1.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
