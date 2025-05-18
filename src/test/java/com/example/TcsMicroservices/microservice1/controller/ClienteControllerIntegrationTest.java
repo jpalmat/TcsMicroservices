26b179ffe2d2cd23aea1ded1ae5e726f77ed661a
Jimmy Palma <jimmypalma@Jimmys-MacBook-Pro.local>
2025-05-18 16:10:23 -0500

    integration test for cliente

---
+++ src/test/java/com/example/TcsMicroservices/microservice1/controller/ClienteControllerIntegrationTest.java
@@ -0,0 +1,60 @@
+package com.example.TcsMicroservices.microservice1.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.example.TcsMicroservices.microservice1.data.Cliente;
+import com.example.TcsMicroservices.microservice1.data.PersonaRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ClienteControllerIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private PersonaRepository clienteRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setUp() {
+        clienteRepository.deleteAll();
+    }
+
+    @Test
+    void createCliente_shouldReturnCreatedClienteAndStatusCreated() throws Exception {
+        Cliente cliente1 = new Cliente();
+        cliente1.setId(1234455678L);
+        cliente1.setName("Jose Lema");
+        cliente1.setGender("Masculino");
+        cliente1.setAge(30);
+        cliente1.setDirection("Otavalo sn y principal");
+        cliente1.setPhone("098254785");
+        cliente1.setEmail("test@gmail.com");
+        cliente1.setPassword("1234");
+        cliente1.setState("True");
+
+        String json = objectMapper.writeValueAsString(cliente1);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/clientes")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(json))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andDo(System.out::println);
+
+        Cliente savedCliente = clienteRepository.findAll().get(0);
+        assertEquals("Jose Lema", savedCliente.getName());
+    }
+}
